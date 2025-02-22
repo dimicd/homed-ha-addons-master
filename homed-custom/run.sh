557219edb5850eaf760fdb5c34b20df3618c6e24
dimicd <59529231+dimicd@users.noreply.github.com>
2025-02-22 18:06:29 +0400

    Update run.sh

--- homed-custom/run.sh
+++ homed-custom/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CONFIG_FILE="/config/homed-automation.conf"
+CONFIG_FILE="/config/homed-custom.conf"
 
 if [ ! -f "${CONFIG_FILE}" ]; then
-	cp /root/homed-automation.conf ${CONFIG_FILE}
+	cp /root/homed-custom.conf ${CONFIG_FILE}
 fi
 
-/usr/bin/homed-automation -c ${CONFIG_FILE}
+/usr/bin/homed-custom -c ${CONFIG_FILE}
